0798f6a27b44ed28a7f5ba4ce4c340905c28cc09
DespiteDeath <Kogut_nikita@mail.ru>
2017-01-05 06:56:09 +0300

    Add files via upload
    Updated removal useless words

--- test.py
+++ test.py
@@ -2,25 +2,44 @@ import numpy as np
 import cv2
 import sys
 from PIL import ImageGrab
-import textract         #you may delete this string
 import pytesseract
+import textract                     #you may delete this string
+import fileinput                    #you may delete this string
+from matplotlib import lines        #you may delete this string
 
+img = ImageGrab.grab(bbox=(10, 1015, 685, 1125)) #510, 440, 1050, 487(for card) #x, y, w, h 5, 505, 340, 555 could be changed for your window size
 
-
-img = ImageGrab.grab(bbox=(10, 1000, 685, 1125)) #x, y, w, h 5, 505, 340, 555 could be changed for your window size
 img_np = np.array(img)
+
 frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-cv2.imshow("frame", frame)      #you may delete this string
+
+cv2.imshow("frame", frame)          #you may delete this string
+
 img.save("screen_capture.png", "PNG")    #.save("screen_capture.png", "PNG")
 
-
+#Deleting "Dealer: "
 sys.stdout=open("test.txt","w")
 print (pytesseract.image_to_string(img))
+sys.stdout.close()
+
+infile = "test.txt"
+outfile = "test_cleaned.txt"
+
+delete_list = ["Dealer: "]
+fin = open(infile)
+fout = open(outfile, "w+")
+for line in fin:
+    for word in delete_list:
+        line = line.replace(word, "")
+    fout.write(line)
+fin.close()
+fout.close()
+####################
 
 sys.stdout.close()
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
 
 #pytesseract.image_to_string(ImageGrab.open('screen_capture.png'))
